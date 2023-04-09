e878cb0d485da5cff5e80e907bdb1cc452c19a32
Edison Duong <edisonvduong@gmail.com>
2023-04-08 20:48:20 -0500

    Skills added

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import AnimateComponent from "../components/AnimateComponent";
+import ItemCard from "../components/ItemCard";
 
-export default function Skills() {
+export default function Skills(props) {
   return (
-    <div>Skills</div>
+    <div className="section white-section">
+      <h1 className="title">Skills</h1>
+      <AnimateComponent
+        animationStyle={"animated-on-visible-appear"}
+      >
+      <ItemCard title={"React JS"} isDesktop={props.isDesktop} />
+      </AnimateComponent>
+      <AnimateComponent
+        animationStyle={"animated-on-visible-appear"}
+      >
+        <ItemCard title={"Grails"} isDesktop={props.isDesktop} />
+      </AnimateComponent>
+    </div>
   )
 }
